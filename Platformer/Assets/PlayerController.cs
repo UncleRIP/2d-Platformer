e6722261cac0252bded63b1ae49ebc4bdd8237a7
Bimmka <bmm_kinder@mail.ru>
2020-10-03 17:08:46 +0400

    Player 1

---
+++ Platformer/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private float jumpForce;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        JumpPlayer();
+
+    }
+    private void MovePlayer()
+    {
+
+    }
+    private void JumpPlayer()
+    {
+
+    }
+}
